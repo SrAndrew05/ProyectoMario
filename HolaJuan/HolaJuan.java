37afcddfdc586fe79d96aa235d65b11142d3a5b0
srandrew <andreiiordache@educarex.es>
2023-11-09 13:47:03 +0100

    Escaner

---
+++ HolaJuan/HolaJuan.java
@@ -0,0 +1,6 @@
+public class holajuan {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Juan")
+	}
+}
---
+++ HolaPedro/HolaPedro.java
@@ -0,0 +1,6 @@
+public class holapedro {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Pedro")
+	}
+}
