faef583e49e2011298b7b87f850f2059a318009f
SrAndrew05 <149794716+SrAndrew05@users.noreply.github.com>
2023-11-10 13:02:09 +0100

    Modificacion en GitHub

--- ProyectoC/src/PrimerosPasos/ProyectoA/src/HolaMundo.java
+++ ProyectoC/src/PrimerosPasos/ProyectoA/src/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Buenos Dias");
+		System.out.println("Esto le he modificado en GitHub");
 	}
 
 }
