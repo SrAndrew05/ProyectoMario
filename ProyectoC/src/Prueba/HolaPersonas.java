1820b763192752bfe82b986f642930b6f7f14569
srandrew <andreiiordache@educarex.es>
2023-11-10 13:23:33 +0100

    Modificacion en GitHub 3

--- ProyectoC/src/Prueba/HolaPersonas.java
+++ ProyectoC/src/Prueba/HolaPersonas.java
@@ -4,8 +4,6 @@ import javax.swing.JOptionPane;
 
 public class HolaPersonas {
 
-
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
